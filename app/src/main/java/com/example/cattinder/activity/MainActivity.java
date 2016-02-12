7faa94fb61199079e59cd5aa0006db1a4cbfd090
Paul Copenhaver <pcopenhaver@gopro.com>
2016-02-12 13:38:58 -0800

    MainFragment added.

--- app/src/main/java/com/example/cattinder/activity/MainActivity.java
+++ app/src/main/java/com/example/cattinder/activity/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.cattinder.activity;
 
 import com.example.cattinder.R;
+import com.example.cattinder.activity.fragment.MainFragment;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    private static final String FRAG_TAG_MAIN = "frag_tag_main";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-  }
+
+        Fragment frag = getFragmentManager().findFragmentByTag(FRAG_TAG_MAIN);
+        if(frag == null) {
+            frag = new MainFragment();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, frag, FRAG_TAG_MAIN)
+                    .commit();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/cattinder/activity/fragment/MainFragment.java
@@ -0,0 +1,20 @@
+package com.example.cattinder.activity.fragment;
+
+import com.example.cattinder.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_hotornot, null);
+    }
+}
